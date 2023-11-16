79787963f9adb65a814d28fe366a521ac221403b
cls315 <camilasaya315@gmail.com>
2023-11-16 16:46:05 -0300

    Primer commit

---
+++ Backend/src/db.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fitevolution`,
+  { logging: false, native: false }
+);
+
+module.exports = {
+  conn: sequelize,
+};
