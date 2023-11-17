95775127d014046737b2afd79eb8ba5409196009
urco <nik_186@hotmail.com>
2023-11-17 01:29:44 -0300

    vistas Hechas

---
+++ Frontend/src/views/Landing.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Landing =()=>{
+    return(
+        <div>
+            <h1>Landing</h1>
+        </div>
+    )
+}
+
+export default Landing;
---
+++ Frontend/src/views/Sesion/FormSesion.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+const FormSesion =()=>{
+    return(
+        <div>
+          
+            <form>
+               <label>Email</label>
+               <input></input>
+
+               <label>Password</label>
+               <input></input>
+
+               <button>Iniciar sesion</button>
+
+        
+
+            </form>
+        </div>
+    )
+}
+
+export default FormSesion;
