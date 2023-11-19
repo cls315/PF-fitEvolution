7997f94a1593d31162bd4ed0119b6ed6f3a5339b
haliercesr <haliercesr@gmail.com>
2023-11-18 21:11:40 -0300

    se agrego el slide en landing-page

--- Frontend/src/views/landing/landing.component.jsx
+++ Frontend/src/views/landing/landing.component.jsx
@@ -1,17 +1,19 @@
 //Components
 import Loading from '../../components/loading/loading.component'
 import SearchBar from '../../components/searchbar/searchbar.component';
+import SliderLanding from '../../components/sliderLanding/sliderLanding.component';
 //Commons imports
 import { useState, useEffect } from "react"
 import { useNavigate } from 'react-router-dom';
 import iconos from '../../images/iconos.png'
 //Styles
 import style from "./landing.module.css"
+import sliderLanding from '../../components/sliderLanding/sliderLanding.component';
 
 
 function Landing(props) {
   const [loading, setLoading] = useState(false)
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   const [showElements, setShowElements] = useState(false);
 
   useEffect(() => {
@@ -26,19 +28,24 @@ function Landing(props) {
   }, []);
 
   return (<div className={style.containerForm}>
-    <SearchBar/>
-    <div className={style.Title}>    
-      <h2 className={style.subTitle1Landing}>Asociate a una de nuestras membresias</h2>
-      <h1 className={style.title1Landing}>COMIENZA AHORA GRATIS!</h1>
-      <h2 className={style.subTitle1Landing}>Disfruta de todos los beneficios de FitRevolution</h2>
-      <img className={style.iconosLanding} src={iconos} alt="iconos"></img>
-      <button className={style.buttonConteinerForm} onClick={() => {
-      setLoading(true)
-      navigate('/home')
-    }}>Asociarse</button>
+    <SearchBar />
+    <div className={style.conteinerTitle}>
+      <div className={style.sliderLandingClass}>
+      <SliderLanding />
+      </div>
+      <div className={style.Title}>
+        <h2 className={style.subTitle1Landing}>Asociate a una de nuestras membresias</h2>
+        <h1 className={style.title1Landing}>COMIENZA AHORA GRATIS!</h1>
+        <h2 className={style.subTitle1Landing}>Disfruta de todos los beneficios de FitRevolution</h2>
+        <img className={style.iconosLanding} src={iconos} alt="iconos"></img>
+        <button className={style.buttonConteinerForm} onClick={() => {
+          setLoading(true)
+          navigate('/home')
+        }}>Asociarse</button>
+      </div>
     </div>
-    
-   
+
+
 
   </div>)
 }
