f9b88da41a43d3276bfa3b8acca2fb9de083701c
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-11-21 18:13:00 -0300

    prueba

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app");
 const PORT = 3001;
 const { conn } = require("./src/db");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     console.log("Server raised");
   });
--- Backend/src/routes/trainersRoutes.js
+++ Backend/src/routes/trainersRoutes.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
 const allTrainerHndls = require("../handlers/TrainersHndls/getAllTrainerHndls");
 const postTrainerHdnls = require("../handlers/TrainersHndls/postTrainerHndls");
-const filteredTrHandl = require ('../handlers/TrainersHndls/filteredTrHandl')
-
+const filterByFocus = require("../controllers/Trainers/filterByfocusApi");
+const filterFocusScore = require("../controllers/Trainers/filterFocusScore");
 
 const trainersRoutes = Router();
 
 trainersRoutes.get("/allTrainer", allTrainerHndls);
-trainersRoutes.get("/filteredTrainers", filteredTrHandl)
 trainersRoutes.post("/", postTrainerHdnls);
-
-
+trainersRoutes.get("/trainerFocus", filterByFocus) // borrar esta linea, de prueba 
+trainersRoutes.get("/filter" , filterFocusScore)
 module.exports = trainersRoutes;
