fa354a33b1989ad77c451a9f5ce5d9a0238ee218
BGBrianGomez <brianrodri_09@hotmail.com>
2023-11-21 19:32:48 -0300

    Boton cerrar sesion

--- Frontend/src/components/navUsuario/navUsuario.jsx
+++ Frontend/src/components/navUsuario/navUsuario.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom"
 import imageLogo from "../../images/imageLogo.jpg"
 import styles from "./navUsuario.module.css"
 
@@ -5,6 +6,9 @@ const NavUsuario = ()=>{
     return(
         <div className={styles.nav}>
             <img src={imageLogo} className={styles.logo}/>
+            <Link to="/">
+            <button className={styles.btn}>Cerrar sesion</button>
+            </Link>
         </div>
     )
 }
