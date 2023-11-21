4a4c317acb6c86985655bbc9176a843ba22fa823
haliercesr <haliercesr@gmail.com>
2023-11-21 10:40:57 -0300

    se agregaron botones en el navbar y panel del entrenador

--- Frontend/src/components/Dashbar/Dashbar.jsx
+++ Frontend/src/components/Dashbar/Dashbar.jsx
@@ -2,21 +2,34 @@ import style from "./Dashbar.module.css"
 import { MdOutlinePayment } from "react-icons/md";
 import { CgGym } from "react-icons/cg";
 import { MdSportsGymnastics } from "react-icons/md";
+import imagelogo from '../../images/imageLogo.jpg';
+import iconsearch from '../SVG/iconsearch.svg'
+import iconbell from '../SVG/iconbell.svg'
+import iconMessage from '../SVG/iconMessage.svg'
+import { Link } from "react-router-dom";
 
 
 
-const DashBar=()=>{
-   return (
-    <div className={style.dashBar}>
-        <button className={style.button}>Home</button>
-        <button className={style.pagos}><MdOutlinePayment/>Pagos</button>
-        <button className={style.entreno}><CgGym/>Entrenamientos</button>
-        <button className={style.depor}><MdSportsGymnastics/>Deportistas</button>
-       
-
-
-    </div>
-   )
+const DashBar = () => {
+    return (
+        <div className={style.dashBar}>
+            <div class={style.dropdown}>
+                <button className={style.buttonPerfil}>CH&#9660;</button>
+                <ul class={style.dropdowncontent}>
+                    <li><a href="#">Mi cuenta</a></li>
+                    <li><a href="#">Ajustes</a></li>
+                    <li><Link to={'/'}>Salir</Link></li>
+                </ul>
+            </div>
+            <img className={style.iconsDashbar} src={iconsearch}></img>
+            <img className={style.iconsDashbar} src={iconbell}></img>
+            <img className={style.iconsDashbar} src={iconMessage}></img>
+            <button className={style.entreno}><CgGym />Entrenamientos</button>
+            <button className={style.depor}><MdSportsGymnastics />Deportistas</button>
+            <button className={style.pagos}><MdOutlinePayment />Pagos</button>
+            <img className={style.logoTraineBar} src={imagelogo} alt="logo" />
+        </div>
+    )
 
 }
 
