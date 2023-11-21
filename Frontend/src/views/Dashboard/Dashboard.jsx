2816fa735f0eaa0da6245c140cf80b4b73533abf
sedanomolina <sedanomolinaraul@gmail.com>
2023-11-20 19:01:07 -0500

    dashboard

--- Frontend/src/views/Dashboard/Dashboard.jsx
+++ Frontend/src/views/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import TrainerSearchbar from '../../components/TrainerSearchbar/TrainerSearchbar';
+import DashBar from './../../components/Dashbar/Dashbar';
+import './Dashboard.css';
 
 const Dashboard = () => {
   return (
     <div className='bg-trainer-board'>
+      <DashBar />
       <div className='d-flex flex-column text-right'>
         <h2>Deportistas activos{' (1)'}</h2>
         <span>
