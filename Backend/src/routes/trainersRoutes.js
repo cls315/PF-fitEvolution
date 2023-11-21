e0cf8848e73a27affe25f0b05d698c8213bf1ec1
FerJuanB <fbatres@gmail.com>
2023-11-21 11:46:53 -0300

    trabajando en filtros, arreglado package.json y agregada dependencia para correcto funcionamiento

--- Backend/src/routes/trainersRoutes.js
+++ Backend/src/routes/trainersRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const allTrainerHndls = require("../handlers/TrainersHndls/getAllTrainerHndls");
 const postTrainerHdnls = require("../handlers/TrainersHndls/postTrainerHndls");
+const filteredTrHandl = require ('../handlers/TrainersHndls/filteredTrHandl')
 
 const trainersRoutes = Router();
 
 trainersRoutes.get("/allTrainer", allTrainerHndls);
+trainersRoutes.get("/filteredTrainers", filteredTrHandl)
 trainersRoutes.post("/", postTrainerHdnls);
 
 module.exports = trainersRoutes;
