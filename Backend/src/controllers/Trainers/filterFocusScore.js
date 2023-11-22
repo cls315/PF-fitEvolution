afce7a661f64dde2f22e34a2f7d1e01d580a90b1
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-11-22 16:25:52 -0300

    filtros

--- Backend/src/controllers/Trainers/filterFocusScore.js
+++ Backend/src/controllers/Trainers/filterFocusScore.js
@@ -1,9 +1,10 @@
 
+// const { Trainer } = require("../../db");
 const getAllTrainer = require("./getAllTrainer");
 
 const filterFocusScore = async (req, res) => {
   try {
-    const { focus, score } = req.query;
+    const { focus, score , sortByFocus, sortByScore } = req.query;
     let trainers = await getAllTrainer();
 
     // Aplicar filtros si se proporcionan
@@ -15,9 +16,33 @@ const filterFocusScore = async (req, res) => {
       trainers = trainers.filter(trainer => trainer.score === score);
     }
 
-
-
+      
+  
+    // Ordenar por ambos filtros si se proporcionan
+    if (sortByFocus) {
+      trainers.sort((a, b) => {
+        if (sortByFocus.toUpperCase() === 'ASC') {
+          return a.focusTr.localeCompare(b.focusTr);
+        } else if (sortByFocus.toUpperCase() === 'DESC') {
+          return b.focusTr.localeCompare(a.focusTr);
+        }
+        // Agrega manejo para otros casos si es necesario
+      });
+    }
+    
+    if (sortByScore) {
+      trainers.sort((a, b) => {
+        if (sortByScore.toUpperCase() === 'ASC') {
+          return a.score - b.score;
+        } else if (sortByScore.toUpperCase() === 'DESC') {
+          return b.score - a.score;
+        }
+        // Agrega manejo para otros casos si es necesario
+      });
+    }
+    
     res.json(trainers);
+
   } catch (error) {
    
     res.status(400).json({ error: 'Error al obtener entrenadores con filtros' });
