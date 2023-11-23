af12d1c0cf332429b579c6302f5bb2d69b81a5ef
sedanomolina <sedanomolinaraul@gmail.com>
2023-11-22 19:50:03 -0500

    created ClientsTable.jsx

--- Frontend/src/components/menuprincipalTainer/menuprincipalTrainer.jsx
+++ Frontend/src/components/menuprincipalTainer/menuprincipalTrainer.jsx
@@ -1,18 +1,20 @@
 import './menuprincipalTrainer.css'
 import TrainerSearchbar from '../../components/TrainerSearchbar/TrainerSearchbar';
+import ClientsTable from '../clientsTable/clientsTable';
 
-export default function menuprincipalTrainer (){
-
-    return(<div className='menuprincipalTrainer'>
-        <div className='d-flex flex-column text-right'>
-          <h3>Deportistas activos{' (1)'}</h3>
-          <span>
-            Bienvenido a tu cuenta de entrenador, aqui puedes ver todos los
-            deportistas
-          </span>
-        </div>
-        <TrainerSearchbar />
-        <div className='conteinerUsersDash'></div>
+export default function menuprincipalTrainer() {
+  return (
+    <div className='menuprincipalTrainer'>
+      <div className='d-flex flex-column text-right'>
+        <h3>Deportistas activos{' (1)'}</h3>
+        <span>
+          Bienvenido a tu cuenta de entrenador, aqui puedes ver todos los
+          deportistas
+        </span>
       </div>
-    )
+      <TrainerSearchbar />
+      <ClientsTable />
+      <div className='conteinerUsersDash'></div>
+    </div>
+  );
 }
