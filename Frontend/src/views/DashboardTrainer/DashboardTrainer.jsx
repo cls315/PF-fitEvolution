0efd0876c0662de3810c70fca973e3a9df81e7b3
haliercesr <haliercesr@gmail.com>
2023-11-27 19:27:41 -0300

    se arreglo un loop en el panel del entrenador

--- Frontend/src/views/DashboardTrainer/DashboardTrainer.jsx
+++ Frontend/src/views/DashboardTrainer/DashboardTrainer.jsx
@@ -25,26 +25,31 @@ const DashboardTrainer = (props) => {
   const [trainer,setTrainer] =useState({})
   const allTrainers=useSelector((state) => state.allTrainers)
   const dispatch= useDispatch()
-  dispatch(getTrainers())
   
-    //firebase
-    const [userSession, setUserSession] = useState(false)
-      //modo escucha de firebase
-     useEffect(()=>{
-       onAuthStateChanged(auth,async(user)=>{    //esta funcion es de firebase se queda en modo escucha cada vez que se carga la aplicacion.
-        if(user){
-          console.log(user.email)
-          
-          console.log(allTrainers)
-          setUserSession(true)
-        } else{
-          setUserSession(false)
-          console.log(user)
-        }
-        })
-      },[allTrainers])
-      //-------------------------*/
-    //---------  
+  
+  //firebase
+  const [userSession, setUserSession] = useState(false)
+  //modo escucha de firebase
+ useEffect(()=>{
+  onAuthStateChanged(auth, async(user)=>{    //esta funcion es de firebase se queda en modo escucha cada vez que se carga la aplicacion.
+    if(user){
+      console.log(user.email)
+      if (allTrainers.length===0)dispatch(getTrainers())
+      setTrainer({...user,email:user.email})
+      console.log(trainer.email)
+      setUserSession(true)
+    } else{
+      setUserSession(false)
+      console.log(user)
+    }
+    })
+
+    return () => {
+     
+    };
+
+  },[allTrainers])
+  //-------------------------*/
 
   const handleMenu = (e) => {
     const nom = e.target.name
@@ -57,7 +62,7 @@ const DashboardTrainer = (props) => {
       userSession ?
         <div className='bg-trainer-board'>
           <DashBar handleMenu={handleMenu} />
-          {menu === "deportes" && <MenuprincipalTrainer />}
+          {menu === "deportes" && <MenuprincipalTrainer trainer={trainer}/>}
           {menu === "pagos" && <PagosprincipalTrainer />}
           {menu === "entrenamientos" && <EntrePrincipalTrainer />}
           <footer className='footerUser'><p>© 2023 FitRevolution </p></footer>
