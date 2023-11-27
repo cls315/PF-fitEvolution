4cfb6b2de3a4d13798479683aa514f5d784c7e37
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-11-27 12:18:19 -0300

    actualizacion

---
+++ Backend/configNodemailer/nodemailer.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer")
+
+//creo el transporter para utiulizar en la ruta para enviar gmail
+const transporter= nodemailer.createTransport({
+    host:"smtp.gmail.com",
+    port:465,
+    secure: true,
+    auth:{
+        user:"fitevolution77@gmail.com",
+        pass: "aekf psrk itpi bcrx",
+    },
+})
+
+
+transporter.verify()
+.then(() => console.log("gmail enviado con exito!"))
+.catch((error) => console.error("Error al verificar el transporter:", error));
+
+module.exports= transporter;
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 const trainersRoutes = require("./trainersRoutes");
 const routinesRoutes = require("./routinesRoutes");
 const clientRoutes = require("./clientRoutes");
-
+const nodemailerRoutes=require("./nodemailer-rutas")
 const router = Router();
 
 router.use("/trainers", trainersRoutes);
 router.use("/routines", routinesRoutes);
 router.use("/clients" , clientRoutes );
+router.use("/nodemailer", nodemailerRoutes)
 
 module.exports = router;
