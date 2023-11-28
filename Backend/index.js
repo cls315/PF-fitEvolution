0b3506a9aa7829e997d53870284ad073e0509345
cls315 <camilasaya315@gmail.com>
2023-11-27 21:00:18 -0300

    Creados componente Pagos Back y Front

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app");
 const PORT = 3001;
 const { conn } = require("./src/db");
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log("Server raised");
   });
---
+++ Backend/src/controllers/Stripe/createCheckoutSession.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const Stripe = require("stripe");
+const cors = require("cors");
+const { WEB_APP_URL } = process.env;
+
+const app = express();
+
+const stripe = new Stripe(
+  "sk_test_51OFhTFEPwEDVOkZID6pJaLBw622WXm02CYoaaVqITgtL5iV1am5OoVrzUL2ycG0W1XTHT9LfHQsigxBu8IJ2yJl400RVo9vsDM"
+);
+
+app.use(cors({ WEB_APP_URL }));
+app.use(express.json());
+
+app.post("/api/checkout", async (req, res) => {
+  try {
+    const { id, amount } = req.body;
+
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "PC Gamer",
+      payment_method: id,
+      confirm: true,
+      return_url: "http://localhost:3000/success",
+    });
+    console.log(payment);
+    res.send({ message: "Succesfull payment" });
+  } catch (error) {
+    console.log(error);
+    res.json({ message: error.raw.message });
+  }
+});
