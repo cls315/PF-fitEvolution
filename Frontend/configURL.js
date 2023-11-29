9422884ab90e639701d4c74f50a4ff6d1b7f7abe
unknown <haliercesr@gmail.com>
2023-11-29 15:16:51 -0300

    Se agrego el registro de usuario

--- Frontend/configURL.js
+++ Frontend/configURL.js
@@ -1,5 +1,5 @@
-export const URLfrontend="http://localhost:5173"
-export const URLSERVER = 'http://localhost:3001'
-//export const URLfrontend="https://pf-fit-evolution.vercel.app";
-//export const URLSERVER='https://backend-tsoa.onrender.com';
+//export const URLfrontend="http://localhost:5173"
+//export const URLSERVER = 'http://localhost:3001'
+export const URLfrontend="https://pf-fit-evolution.vercel.app";
+export const URLSERVER='https://backend-tsoa.onrender.com';
 
